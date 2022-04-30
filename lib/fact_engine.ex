6da1f510705e8ac9148b7b5e503b44f62d4ae20c
Noah Betzen <noah@nezteb.net>
2022-04-29 23:38:40 -0500

    Another hour, WIP is ugly but is close to working!

--- lib/fact_engine.ex
+++ lib/fact_engine.ex
@@ -20,13 +20,88 @@ defmodule FactEngine do
       content
       |> String.trim()
       |> String.split("\n")
+      # Parse each file
       |> Enum.map(fn line ->
         case FactEngine.Parser.fact(line) do
-          {:ok, results, _, _, _, _} -> 
-            FactEngine.Utils.combine_tuple_list(results)
-          {:error, message, string, _, _, _} -> "#{message}: #{string}"
+          {:ok, results, _, _, _, _} ->
+            {:ok, FactEngine.Utils.combine_tuple_list(results)}
+
+          {:error, message, string, _, _, _} ->
+            IO.inspect("#{file}: #{message}: #{string}")
         end
       end)
+      # Discard any invalid lines
+      |> Enum.filter(fn tuple ->
+        case tuple do
+          {:ok, _} -> true
+          _ -> false
+        end
+      end)
+      # Extract the results
+      |> Enum.map(fn ok_tuple ->
+        {:ok, results} = ok_tuple
+        results
+      end)
+      |> Enum.reduce(%{}, fn line, logic_table ->
+        IO.inspect(line, label: "Line")
+
+        result = case line do
+          %{command: :input, property: query_property, subject: subjects} ->
+            # TODO: Make this a function to clean it up
+            {_, new_map} = FactEngine.Utils.add_item_to_map(logic_table, query_property, subjects)
+            new_map
+
+          %{
+            command: :query,
+            property: query_property,
+            subject: query_subjects,
+            variable: query_variables
+          } ->
+            # TODO: Account for subjects and variables in queries...
+            logic_table
+
+          %{command: :query, property: query_property, subject: query_subjects} ->
+            # returns list of lists
+            subjects = Map.get(logic_table, query_property)
+
+            Enum.map(subjects, fn subject ->
+              IO.inspect({query_subjects, subject}, label: "Query, actual")
+              is_subset = MapSet.subset?(MapSet.new([query_subjects]), MapSet.new(subject))
+              IO.puts("#{is_subset}")
+            end)
+
+            logic_table
+
+          %{command: :query, property: query_property, variable: query_variables} ->
+            subjects = Map.get(logic_table, query_property)
+
+            # TODO: How to handle multiple query variables?
+            query_result =
+              case subjects do
+                nil ->
+                  false
+
+                _ ->
+                  Enum.map(subjects, fn subject ->
+                    [string_subject] = subject
+                    IO.puts("#{query_variables}: #{string_subject}")
+                    # IO.inspect({query_variables, subject}, label: "Query, actual")
+                    
+
+                    # is_subset = MapSet.subset?(MapSet.new([query_subjects]), MapSet.new(subject))
+                    # IO.puts("#{is_subset}")
+                  end)
+              end
+
+            IO.inspect(query_result, label: "Query result")
+            logic_table
+        end
+
+        IO.puts("")
+        result
+      end)
     end)
+
+    # nil
   end
 end
--- lib/utils.ex
+++ lib/utils.ex
@@ -1,18 +1,39 @@
 defmodule FactEngine.Utils do
   import NimbleParsec
   
+  # If item is a string, prepends it to the list
+  # If item is a list, combine the two lists
+  def add_item_to_map(map, key, item) do
+    Map.get_and_update(map, key, fn current ->
+      # TODO: There must be a better way to do this (maybe with a `with`?)
+      case current do
+        nil when is_binary(item) ->
+          {key, [[item]]}
+        nil when is_list(item) ->
+          {key, [item]}
+        _ when is_binary(item) ->
+          {key, [[item] | current]}
+        _ when is_list(item) ->
+          {key, [item | current]}
+      end
+    end)
+  end
+  
+  # Turns a list of tuples like {:atom, "string"} and turns them into a map,
+  # combining similarly keyed strings into lists
   def combine_tuple_list(list) do
     Enum.reduce(list, %{}, fn tuple, map ->
       {key, value} = tuple
     
+      # This will update a key whether the incoming value is a single string or a list
       {_, new_map} = Map.get_and_update(map, key, fn current ->
         case current do
           nil ->
             {key, value}
-          currentList when is_list(currentList) ->
-            {key, [value | currentList]}
-          currentString when is_binary(currentString) ->
-            {key, [currentString, value]}
+          _ when is_binary(current) ->
+            {key, [current, value]}
+          _ when is_list(current) ->
+            {key, [value | current]}
         end
       end)
     
